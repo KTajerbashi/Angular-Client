374fa7c3f3b29dc8c07722f43b351c1531b957a8
KTajerbashi <kamrantajerbashi@gmail.com>
2026-05-27 13:37:03 +0330

    #Feature Config HttpClient and Register

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,10 +8,19 @@ import {
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { MockUserDataService } from './core/services/mock-user-data-service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
+    provideHttpClient(withFetch()), // Add this
+    // In-memory web API configuration
+    {
+      provide: 'USE_IN_MEMORY_API',
+      useValue: true,
+    },
+    MockUserDataService,
     provideRouter(
       routes,
       withComponentInputBinding(), // Bind route params to @Input()
